5aabcc9b0a4aa03d9ca8452f089a3c5249ce6f6b
Victoria Frantsuh <eboko1@gmail.com>
2017-01-15 18:33:54 +0200

    adding class computer

--- src/Computer.java
+++ src/Computer.java
@@ -1,11 +1,45 @@
+import java.awt.*;
+
 /**
  * Created by Vika on 31.12.2016.
  */
 public class Computer {
-    private int y;
+    private GamePanel field;
 
-    public Computer(){
+    private int y=Pong.WINDOWS_HEIGHT/2;
 
-        
+
+    private  int yVelocity=0;
+    private int width=15;
+    private int height=40;
+    public Computer(GamePanel game){
+    }
+
+    public void update(){
+
+        y = y + yVelocity;
+
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.YELLOW);
+        g.fillRect(Pong.WINDOWS_WIDTH-(35-width),y,width,height);
+
+    }
+
+    public int getX() {
+        return 35;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
